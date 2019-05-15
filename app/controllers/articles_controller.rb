3537332a9b59d866de2e7f89ad22469c33964b81
Claudia Tavares <claudia.tavares@fabernovel.com>
2019-05-15 17:24:33 +0200

    feat: add destroyy method and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
 		 @article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.new(article_params)
 
@@ -20,6 +24,23 @@ class ArticlesController < ApplicationController
 			render 'new'
 			# redirect_to action: "new"
 		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+
+		@article.destroy
+		redirect_to articles_path
 	end
 
 	private
